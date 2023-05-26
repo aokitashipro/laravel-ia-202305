349d188783153e7f162b746d2555642301d0f651
aokitashipro <webmocha2@gmail.com>
2023-05-26 09:21:23 +0900

    readme追記

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-202305 Laravel研修
+202305 Laravel研修<br>
+
+## ローカル確認方法
+git clone https://github.com/aokitashipro/laravel-ia-202305.git
+
+
+ブランチ指定時は<br>
+git clone -b ブランチ名 https://github.com/aokitashipro/laravel-ia-202305.git
+
+
+cd laravel-ia-202305
+
+
+## XAMPP/MAMPの場合
+cp .env.example .env
+
+vi .env (DB情報追記)
+
+php artisan key:generate
+
+composer install
+
+npm install
+
+php artisan serve
+
+npm run dev
+
+## Docker/Sailの場合
+cp .env.example .env
+
+./vendor/bin/sail up -d
+
+./vendor/bin/sail artisan key:generate
+
+./vendor/bin/sail composer install
+
+./vendor/bin/sail npm install
+
+./vendor/bin/sail npm run dev 
+
--- app/Models/Furniture.php
+++ app/Models/Furniture.php
@@ -4,8 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use App\Const\Prefecture;
 
 class Furniture extends Model
 {
     use HasFactory;
+
+    // 今回は全部まとめてますが
+    // 個別にscopeつくる場合もよくあります
+    public function scopeSearch(Builder $query, $request){
+        
+        // $request->prefecture;
+        if($request->prefecture !== 'all'){        
+        $query->where('prefecture', $request->prefecture);
+        }
+        // $request->order;
+        if($request->order === 'latest'){        
+            $query->orderBy('created_at', 'desc');
+        } else {
+            $query->orderBy('review', 'desc');
+        }
+    
+        // $request->min_price;
+        // $request->max_price;
+        if(!is_null($request->min_price) && !is_null($request->max_price) )
+        {
+            $query->whereBetween('price', [$request->min_price, $request->max_price]);
+        } else if (!is_null($request->min_price)){
+            $query->where('price' >= $request->min_price );
+        } else if(!is_null($request->min_price)){
+            $query->where('price' <= $request->max_price );
+        } else {
+            $query;
+        }
+
+        $query->where('is_visible', true);
+    }
+
 }
