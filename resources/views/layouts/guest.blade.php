2e868b5fab730b85072d4f9945e4b469291dc79e
aokitashipro <webmocha2@gmail.com>
2023-06-15 08:50:31 +0900

    ファイル説明追記

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -24,6 +24,7 @@
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
+                <!-- スロット・・差し込み口 別のファイルが表示できる -->
             </div>
         </div>
     </body>
